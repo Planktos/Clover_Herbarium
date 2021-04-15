449d133bf8c9855142db96a553a394f4a34a0b25
Planktos <kellylynnrobinson@gmail.com>
2021-04-15 15:08:16 -0500

    *iteritive matching locality from herbarium record to city/town name in US.Rdata
    *return list with herbarium record including lat/lon coordinates of town

--- geocoding.R
+++ geocoding.R
@@ -1,48 +1,62 @@
 library(tidyverse)
 library(ggmap)
+library(plyr)
 
 
-g <- read.csv("Nevada_Geocode.csv")
-t <- g[1,4]
-
-s <-  d[1,c("state")]
-
-m <-  regexpr(pattern,t)
+g <- read.csv("Nevada_Geocode.csv", stringsAsFactors = F)
+colnames(g) <- tolower(colnames(g))
+g$x <- NULL
 
 
-l <- regmatches(t, gregexpr("\\b[A-Z]\\w+",t))
+#Match text string in object 'l' from Locallity filed to town/city names in 'USA' file
+load("US.Rdata")
+head(d)
+la <- d[d$`admin1_code `== "LA",]
+rm(d)
+
+las <- la[,c("name","latitude","longitude")]
 
 
-l$locality[3]
+#loop through the herbarium sample list row by row and see if the locality matches with the us cities/towns data
+geo.code.list = list()
 
-x <- paste(l$locality[3], s, sep = ",")
+for (i in 1:nrow(g)) {
+  
+  x <- g[i,]
+  
+  locallity <- x$locallity
+  
+  #m <-  regexpr(pattern = pattern,t)
+  
+  #extracts text strings with capitalized letters
+  upper.strings <- regmatches(locallity, gregexpr("\\b[A-Z]\\w+", locallity))
+  upper.strings <- as.data.frame(upper.strings)
+  colnames(upper.strings) <- c("locallity")
+  upper.strings$locallity <- as.character(upper.strings$locallity)
 
-g$results[[1]]$geometery$bound$southwest$lat
+  match.df <- ddply(.data = upper.strings, .variables = c("locallity"), function(m){
+    
+    #change 'upper.strings' to 'm')
+  
+    match <- la[grepl(pattern = paste("^", m,"$", sep=""), x = la$name),]
+    
+    match.geo <- match[,c("name","latitude","longitude")]
+    
+    match.geo.sample <- cbind(match.geo, x)
+    match.geo.sample <- match.geo.sample[,c(4:17,1:3)]
+    
+    return(match.geo.sample)
+    
+  }, .inform = T, .progress = "text")
+  
+  match.df <- match.df[!is.na(match.df$name),]
+  geo.code.list[[i]] <- as.list(match.df)
 
-g$results[[1]]$geometery$bound$southwest$lng
+  }
+  
+  big_data <- data.table::rbindlist(geo.code.list)
+  
+  big_data = big_data[!duplicated(big_data$cpingsample.),]
 
-
-pattern <-  "\\b[A-z]\\w+"
-
-
-geocodeQueryCheck(userType = "free")
-
-install.packages("RVerbalExpressions")
-library(RVerbalExpressions)
-strings = c('US 110','LA 690','la. 22', 'I10')
-
-expr =  rx_alpha() 
-stringr::str_extract_all(strings,expr)  
-
-expr =  rx_digit() 
-stringr::str_extract_all(strings,expr)  
-
-
-expr =  rx_alpha()  %>%  rx_word() %>% rx_alpha() 
-stringr::str_extract_all(strings,expr) 
-
-expr
-
-"[A-z]\\d+"
-
-sksks
+  save(big_data, file="LA_Herb_GeoCode_Cities.Rdata")
+  
